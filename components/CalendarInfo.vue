b1f21ecaa4c163c01dba84a1c49767abe27c5b56
Dallas Hoffman <contact@dallashoffman.com>
2020-07-29 21:17:52 -0400

    Calendar link opens in new tab

--- components/CalendarInfo.vue
+++ components/CalendarInfo.vue
@@ -2,7 +2,7 @@
     <lazy-section id="calendar" class="calendar-info">
         <stroke-heading>Class Schedule</stroke-heading>
         <p>View the full calendar of upcoming classes on Google Calendars.</p>
-        <a href="https://calendar.google.com/calendar/b/6?cid=ZXJpY2dyaWZmaW50a2RAZ21haWwuY29t" rel="noopener">
+        <a href="https://calendar.google.com/calendar/b/6?cid=ZXJpY2dyaWZmaW50a2RAZ21haWwuY29t" rel="noopener" target="_blank">
             <fa icon="arrow-circle-right" fixed-width></fa>
             Calendar
         </a>
