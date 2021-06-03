6fe367ab6ab96bd6e56eddf9cc73cdec6c812137
Dallas Hoffman <dallashoff@gmail.com>
2021-06-02 21:11:53 -0400

    Keep carousel active thumbnail in view

--- components/CarouselThumbs.vue
+++ components/CarouselThumbs.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="carousel-thumbs">
-        <figure v-for="(photo, index) in carouselPhotos" :key="photo.path" @click="$emit('viewing-photo', index)" @keyup.enter="$emit('viewing-photo', index)" class="carousel-thumbs__thumbnail" :class="{'carousel-thumbs__thumbnail--active': photoIndex === index}" tabindex="0">
+        <figure v-for="(photo, index) in carouselPhotos" :key="photo.path" :ref="'thumb' + index" @click="$emit('viewing-photo', index)" @keyup.enter="$emit('viewing-photo', index)" class="carousel-thumbs__thumbnail" :class="{'carousel-thumbs__thumbnail--active': photoIndex === index}" tabindex="0">
             <img :src="require('~/assets' + photo.path + '?resize&size=300')" :alt="photo.alt" />
         </figure>
     </section>
@@ -9,7 +9,41 @@
 <script>
 export default {
     name: 'CarouselThumbs',
-    props: ['carouselPhotos', 'photoIndex']
+    props: ['carouselPhotos', 'photoIndex'],
+    watch: {
+        photoIndex(n, o) {
+            // Keep active thumbnail in view by scrolling container
+            var animLengthMs = 200;
+            var offscreenTolerance = 20;
+            var container = this.$el;
+            var thumbnail = this.$refs['thumb' + n][0];
+            var scrollPosition = container.scrollLeft;
+            var animStartTime = null;
+            var animScrollTo = null;
+            var isOffscreenRight = thumbnail.offsetLeft + offscreenTolerance > scrollPosition + container.offsetWidth;
+            var isOffscreenLeft = thumbnail.offsetLeft + thumbnail.offsetWidth - offscreenTolerance < scrollPosition;
+            if (isOffscreenRight || isOffscreenLeft) {
+                animScrollTo = thumbnail.offsetLeft;
+                if (window) window.requestAnimationFrame(animateScroll);
+            }
+            // Animation for scrolling
+            function animateScroll(timestamp) {
+                if (animStartTime === null) animStartTime = timestamp;
+                var elapsed = timestamp - animStartTime;
+                var positionDiff = Math.abs(scrollPosition - animScrollTo);
+                var direction = 1;
+                var stopMethod = 'min';
+                if (animScrollTo < scrollPosition) {
+                    direction = -1;
+                    stopMethod = 'max';
+                }
+                container.scrollLeft = Math[stopMethod](animScrollTo, scrollPosition + ((elapsed / animLengthMs) * positionDiff * direction));
+                if (elapsed <= animLengthMs) {
+                    window.requestAnimationFrame(animateScroll);
+                }
+            }
+        }
+    }
 }
 </script>
 
