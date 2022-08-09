464686b8dfcd9f85a10e6f25c50bc899c3c87a5f
Dallas Hoffman <dallashoff@gmail.com>
2022-08-08 21:55:33 -0400

    Class schedule section hiding fix

--- pages/classes/_cls/index.vue
+++ pages/classes/_cls/index.vue
@@ -12,7 +12,7 @@
             </p>
             <class-links class="page-classes__links" :class-content="content"></class-links>
         </section>
-        <section id="schedule" v-if="schedule && Array.from(schedule).length">
+        <section id="schedule" v-if="schedule && schedule.size">
             <h2 class="page-classes__heading">
                 Class Schedule
             </h2>
